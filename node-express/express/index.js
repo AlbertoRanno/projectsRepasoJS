238b6ca38ce653fb97ce02f5e8abd8a72ca77b1e
Alberto Ranno <albertoranno@hotmail.com>
2022-10-21 13:49:50 -0300

    distintas formas de instalación de un paquete / @version / --save-dev para DevDependencies

---
+++ node-express/express/index.js
@@ -0,0 +1,7 @@
+/* Para instalar un versión específica de un paquete se usa el "@":
+npm install express@4.18.2 */
+
+/* Para instalar un paquete como dependencia, pero de desarrollo (development):
+npm install express --save-dev 
+y figurará en package.json como: "devDependencies": { "express": "^4.18.2" } 
+las devDependencies, solo se necesitan para crear la App, y una vez terminada, listo */
